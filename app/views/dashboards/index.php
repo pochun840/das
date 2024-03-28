f8f74ebf975f53f253530a4d8e7177e707f46cf0
pochun840 <0840@kilews.com.tw>
2024-03-28 13:36:52 +0800

    檔案還原

--- app/views/dashboards/index.php
+++ app/views/dashboards/index.php
@@ -38,7 +38,19 @@
             </div>
         </div>
 <script>
-
+    function language_change() {
+    var language = event.target.id;
+    $.ajax({
+      type: "POST",
+      url: "?url=Dashboards/change_language",
+      data: {'language':language},
+      dataType: "json",
+      encode: true,
+      async: false,//等待ajax完成
+    }).done(function (data) {//成功且有回傳值才會執行
+        location.reload();
+    });
+}
     // WEB-iDAS FOR GTCS
     const text = document.querySelector('.text');
     const charArr = text.textContent.split('');
--- app/views/login/index.php
+++ app/views/login/index.php
@@ -16,6 +16,21 @@
 </div>
 
   <script>
+
+    function language_change() {
+        var language = event.target.id;
+        $.ajax({
+        type: "POST",
+        url: "?url=Dashboards/change_language",
+        data: {'language':language},
+        dataType: "json",
+        encode: true,
+        async: false,//等待ajax完成
+        }).done(function (data) {//成功且有回傳值才會執行
+            location.reload();
+        });
+    }
+
     $(document).ready(function () {
         <?php 
             if($data['error_message'] != ''){
